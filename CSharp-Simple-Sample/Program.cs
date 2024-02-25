6286f0536509a724d7398c42f119ee286bbfd4c1
Alex Pimenta <aspepper@hotmail.com>
2024-02-25 15:53:46 -0300

    first commit

---
+++ CSharp-Simple-Sample/Program.cs
@@ -0,0 +1,30 @@
+﻿// Definition of the Person class
+class Person
+{
+    // Person class properties
+    public required string Name { get; set; }
+    public int Age { get; set; }
+
+    // Method for introducing the person
+    public void Greeting()
+    {
+        Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating an instance of the Person class
+        Person person1 = new()
+        {
+            // Assigning values to person1 properties
+            Name = "Alex Pimenta",
+            Age = 51
+        };
+
+        // Chamando o método Apresentar da pessoa1
+        person1.Greeting();
+    }
+}
